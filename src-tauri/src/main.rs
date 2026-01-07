ff2cf835d4068b2219d8ec44ff8b35581e674709
Wike-CHI <3132812664@qq.com>
2026-01-07 22:58:08 +0800

    feat: 使用 Manager trait 获取 WebviewWindow 并打开开发工具

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,9 +1,12 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::Manager;
+
 #[tauri::command]
 async fn open_devtools(window: tauri::Window) {
-    if let Err(e) = window.open_devtools() {
-        eprintln!("Failed to open devtools: {}", e);
+    // 使用 Manager trait 获取 WebviewWindow
+    if let Some(webview) = window.get_webview_window("main") {
+        let _ = webview.open_devtools();
     }
 }
 
